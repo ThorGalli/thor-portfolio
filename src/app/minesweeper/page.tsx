e6d20e1520aaefeb5b699841940e72eae2a07d46
Thor Galli <gallithor@gmail.com>
2023-10-13 17:01:40 -0300

    Add Mine Sweeper button and page

---
+++ src/app/minesweeper/page.tsx
@@ -0,0 +1,11 @@
+import NavbarLayout from '@/layouts/navbar'
+
+export default function MineSweeper() {
+  return (
+    <NavbarLayout>
+      <div>
+        <h1>MineSweeper</h1>
+      </div>
+    </NavbarLayout>
+  )
+}
