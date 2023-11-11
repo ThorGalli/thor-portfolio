b9f875ee87a1843b35cb5d73777e62bc713d43a8
Thor Galli <gallithor@gmail.com>
2023-11-11 02:03:53 -0300

    :lipstick: Reverse items order

--- src/components/shops/resourceList.tsx
+++ src/components/shops/resourceList.tsx
@@ -8,10 +8,12 @@ export default function ResourceList({ tight }: { tight?: boolean }) {
   const { items } = useClickerContext()
 
   const itemBars = useMemo(() => {
-    return Object.values(items).map((item, index, list) => {
-      const isVisible = index === 0 || list[index - 1]?.amount > 0
-      return <BuyableBar key={item.id} buyable={item} visible={isVisible} />
-    })
+    return Object.values(items)
+      .map((item, index, list) => {
+        const isVisible = index === 0 || list[index - 1]?.amount > 0
+        return <BuyableBar key={item.id} buyable={item} visible={isVisible} />
+      })
+      .toReversed()
   }, [items])
 
   return <BuyableList title={'Resources'} list={itemBars} tight={tight} />
