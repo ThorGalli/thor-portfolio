98cb517251d4cb25f2691e8c3a9920f1e9b08c6c
Thor Galli <gallithor@gmail.com>
2023-11-21 19:01:06 -0300

    :children_crossing: Add tutorial at the start of the game

--- src/features/clicker/components/mainCoin.tsx
+++ src/features/clicker/components/mainCoin.tsx
@@ -2,24 +2,52 @@
 import { useClickerContext } from '@/features/clicker/useClickerContext'
 import BaseCoin from '../../../components/coins/baseCoins'
 import useClickerCalculations from '@/features/clicker/hooks/useClickerCalculations'
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
+import useTwoStagesAnimation from '@/hooks/useTwoStagesAnimation'
 
 export default function MainCoin() {
-  const { resourceIncome, autoIncome, totalCoins, clicksIncome, onClickCoin } =
-    useClickerContext()
+  const {
+    resourceIncome,
+    autoIncome,
+    totalCoins,
+    clicksIncome,
+    onClickCoin,
+    clicks,
+  } = useClickerContext()
   const { short } = useClickerCalculations()
 
   function handleClick() {
     onClickCoin()
   }
   const [isOpen, setIsOpen] = useState(false)
-
+  const clicksTooFew = clicks < 10
+  const [showTutorial, setShowTutorial] = useState(clicksTooFew)
   const toggleAccordion = () => {
     setIsOpen(!isOpen)
   }
 
+  const { animateClose, showContent, styles } = useTwoStagesAnimation({
+    animStyles: {
+      tutorialStyles: {
+        inactive: { opacity: 0, maxHeight: 0, marginBottom: 0 },
+        active: { opacity: 1, maxHeight: 20, marginBottom: '0.5rem' },
+      },
+    },
+    durationInMs: 1000,
+    isActive: showTutorial,
+  })
+
+  useEffect(() => {
+    if (!clicksTooFew) {
+      animateClose(() => setShowTutorial(false))
+      return
+    }
+    setShowTutorial(true)
+  }, [clicksTooFew])
+
   const totalIncome =
     Math.round((resourceIncome + autoIncome + clicksIncome) * 100) / 100
+
   return (
     <div className="flex w-full flex-col items-center gap-2 rounded-t-[14px] bg-slate-800 p-2">
       <div className="flex w-full flex-col items-center justify-start gap-2 rounded-md bg-slate-950 pb-2 lg:w-full">
@@ -44,7 +72,7 @@ export default function MainCoin() {
               <p>+{short(resourceIncome, 2)}/s</p>
             </div>
             <div className="flex w-full justify-between">
-              <p>&nbsp;L Auto Clicker:&nbsp;</p>
+              <p>&nbsp;L Clicker Bots:&nbsp;</p>
               <p>+{short(autoIncome, 2)}/s</p>
             </div>
             <div className="flex w-full justify-between">
@@ -54,14 +82,22 @@ export default function MainCoin() {
           </section>
         </button>
 
-        <button
-          id="coin"
-          onClick={handleClick}
-          style={{ transition: 'all 0.1s ease-in-out' }}
-          className="mt-2 cursor-pointer overflow-hidden rounded-full transition-all hover:scale-110 active:scale-95"
-        >
-          <BaseCoin />
-        </button>
+        <div className="flex flex-col items-center">
+          {showTutorial && (
+            <p style={styles.tutorialStyles}>
+              <span className="text-yellow-400">Click</span> the Coin to earn
+              coins!
+            </p>
+          )}
+          <button
+            id="coin"
+            onClick={handleClick}
+            style={{ transition: 'all 0.1s ease-in-out' }}
+            className="mt-2 cursor-pointer overflow-hidden rounded-full transition-all hover:scale-110 active:scale-95"
+          >
+            <BaseCoin />
+          </button>
+        </div>
 
         <p className="text-2xl text-slate-400">
           Total Coins:{' '}
