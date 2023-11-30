80fa8d0fa6bcbed912700e3466773e861f30704f
Thor Galli <gallithor@gmail.com>
2023-11-30 13:17:14 -0300

    :ambulance: Try to fix me query

--- src/contexts/useMe.tsx
+++ src/contexts/useMe.tsx
@@ -9,24 +9,49 @@ import React, {
 
 export type MeProps = {
   showAdmin: boolean
+  loading: boolean
   me: unknown
 }
 
 export function MeProvider({ children }: { children: React.ReactNode }) {
   const [showAdmin, setShowAdmin] = useState(false)
+  const [me, setMe] = useState(null)
+  const [loading, setLoading] = useState(true)
+  const [checkAgain, setCheckAgain] = useState(false)
 
   const { data, status } = useSession()
 
   async function fetchPermission() {
+    setLoading(true)
     try {
       const response = await (
-        await fetch('/api/users/me', { method: 'GET' })
+        await fetch('/api/users/me', { method: 'GET', cache: 'no-store' })
       ).json()
-      if (response?.data?.admin) setShowAdmin(true)
+      if (response?.data) {
+        setShowAdmin(response.data.admin)
+        setMe(response.data)
+      } else {
+        console.log('no data')
+        setTimeout(() => {
+          setCheckAgain(true)
+        }, 5000)
+        setShowAdmin(false)
+        setMe(null)
+      }
     } catch (error) {
       console.error(error)
+    } finally {
+      setLoading(false)
     }
   }
+
+  useEffect(() => {
+    if (checkAgain) {
+      console.log('check again')
+      setCheckAgain(false)
+      fetchPermission()
+    }
+  }, [checkAgain])
 
   useEffect(() => {
     if (status === 'authenticated') {
@@ -35,8 +60,8 @@ export function MeProvider({ children }: { children: React.ReactNode }) {
   }, [status])
 
   const contextValues = useMemo(() => {
-    return { showAdmin, me: data }
-  }, [showAdmin, data])
+    return { showAdmin, me, loading }
+  }, [showAdmin, me, loading])
 
   return (
     <MeContext.Provider value={contextValues}>{children}</MeContext.Provider>
@@ -45,6 +70,7 @@ export function MeProvider({ children }: { children: React.ReactNode }) {
 
 const MeContext = createContext<MeProps>({
   showAdmin: false,
+  loading: true,
   me: null,
 })
 
