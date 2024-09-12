32da14f2a72ee08917c214aeb5a5f4fc80baa3a9
Thor Galli <gallithor@gmail.com>
2024-09-12 10:51:36 -0300

    Update metadata

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,8 +4,8 @@ import { ProvidersWrapper } from '@/contexts/providersWrapper'
 import ToastHolder from '@/components/toast/toastHolder'
 
 export const metadata: Metadata = {
-  title: 'Thor Galli',
-  description: 'Portfolio + Games',
+  title: 'Thor Galli - Full Stack Developer React, Java, Node.js, Typescript',
+  description: `Thor Galli's Portfolio. Full Stack Developer. ReactJS, Java, Node.js, Typescript. Games: Clicker and Minesweeper`,
 }
 
 export default function RootLayout({
